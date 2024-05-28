b4660f68e5effc6da5570645c8eef7aa280011f5
tuantu <tunt4@vng.com.vn>
2024-05-28 21:22:53 +0700

    Implement id selector

--- register.go
+++ register.go
@@ -4,6 +4,7 @@ import (
 	"login/entity"
 	"login/internal/utility"
 	"login/mq"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,8 @@ import (
 type registerHandler struct {
 	mq    mq.MessageQueue
 	idGen utility.IdGenerator
+	resp  map[string]entity.RegisterResponse
+	mu    sync.Mutex
 }
 
 // Build message and send to message queue for processing
@@ -53,15 +56,34 @@ func (rh *registerHandler) handle(c *gin.Context) {
 }
 
 func (rh *registerHandler) getResponse(id string) entity.RegisterResponse {
-	return entity.RegisterResponse{
-		Code:   200,
-		Status: "OK",
+	for retry := 0; retry < 3; retry++ {
+		resp, ok := rh.resp[id]
+		if ok {
+			rh.mu.Lock()
+			delete(rh.resp, id)
+			rh.mu.Unlock()
+
+			return resp
+		}
 	}
+	return entity.RegisterResponse{}
 }
 
-// TODO: Implement selector
+// Basic implementation of message queue
 func (rh *registerHandler) initSelector() {
+	rh.resp = make(map[string]entity.RegisterResponse)
+	rh.mu = sync.Mutex{}
+	rh.mq.Subscribe("register_response", func(msg interface{}) error {
+		resp, ok := msg.(entity.RegisterResponseMsg)
+		if !ok {
+			return nil
+		}
 
+		rh.mu.Lock()
+		rh.resp[resp.Id] = resp.Response
+		rh.mu.Unlock()
+		return nil
+	})
 }
 
 func newRegisterHandler(mq mq.MessageQueue, idGen utility.IdGenerator) *registerHandler {
